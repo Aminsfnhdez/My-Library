07743f078c9cda128d29ae494c1ae4d204eba4c7
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-03-30 01:09:11 -0500

    corrección de primeNG co.existencia tailwindcss

--- src/app/components/anterooms/anterooms.component.ts
+++ src/app/components/anterooms/anterooms.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClass } from 'primeng/styleclass';
+import { Drawer } from 'primeng/drawer';
 
 @Component({
   selector: 'app-anterooms',
-  imports: [ButtonModule],
+  imports: [DrawerModule, ButtonModule, Ripple, AvatarModule, StyleClass],
   templateUrl: './anterooms.component.html',
   styleUrl: './anterooms.component.scss'
 })
 export class AnteroomsComponent {
+  @ViewChild('drawerRef') drawerRef!: Drawer;
 
+    closeCallback(e: any): void {
+        this.drawerRef.close(e);
+    }
+
+    visible: boolean = false;
 }
