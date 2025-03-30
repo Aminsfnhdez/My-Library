790f60b2d38f4dee90a2a2a75a82705e3c310b0b
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-03-30 00:26:13 -0500

    Anteroom and primeNG

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AnteroomsComponent } from './components/anterooms/anterooms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'anterooms',
+        pathMatch: 'full'
+    },
+    {
+        path: 'anterooms',
+        component: AnteroomsComponent,
+    }
+];
---
+++ src/app/components/anterooms/anterooms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-anterooms',
+  imports: [ButtonModule],
+  templateUrl: './anterooms.component.html',
+  styleUrl: './anterooms.component.scss'
+})
+export class AnteroomsComponent {
+
+}
