ce23898855d895216d47719d2c9c499d86323732
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-03-30 16:15:39 -0500

    anterooms complete

--- src/app/components/anterooms/anterooms.component.ts
+++ src/app/components/anterooms/anterooms.component.ts
@@ -1,23 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
-import { DrawerModule } from 'primeng/drawer';
+import { Component } from '@angular/core';
+
 import { ButtonModule } from 'primeng/button';
-import { Ripple } from 'primeng/ripple';
-import { AvatarModule } from 'primeng/avatar';
-import { StyleClass } from 'primeng/styleclass';
-import { Drawer } from 'primeng/drawer';
+
 
 @Component({
   selector: 'app-anterooms',
-  imports: [DrawerModule, ButtonModule, Ripple, AvatarModule, StyleClass],
+  imports: [ButtonModule ],
   templateUrl: './anterooms.component.html',
   styleUrl: './anterooms.component.scss'
 })
 export class AnteroomsComponent {
-  @ViewChild('drawerRef') drawerRef!: Drawer;
-
-    closeCallback(e: any): void {
-        this.drawerRef.close(e);
-    }
-
-    visible: boolean = false;
+  
 }
