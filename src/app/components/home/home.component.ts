03d8fed2105b3d16f101874830f1ad2f613eeee0
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-04-03 22:28:58 -0500

    ajustes visuales

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 ;
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "../header/header.component";
 import { NabVarComponent } from "../nab-var/nab-var.component";
 import { FooterComponent } from "../footer/footer.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, NabVarComponent, FooterComponent],
+  imports: [CommonModule, NabVarComponent, FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
