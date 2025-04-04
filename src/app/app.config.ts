fb16bc4c074a3050f397bb5b01d58445a1edc640
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-04-03 20:44:09 -0500

    configuración del servicio Libro y configuración de FireBase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,9 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,5 +25,17 @@ export const appConfig: ApplicationConfig = {
         },
       },
     }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'my-library-caa60',
+        appId: '1:2692045857:web:e6c2ce54a5ed4da1f99760',
+        storageBucket: 'my-library-caa60.firebasestorage.app',
+        apiKey: 'AIzaSyCwZmUqxojDZG9bXML_bme3JXf-xvBI2sU',
+        authDomain: 'my-library-caa60.firebaseapp.com',
+        messagingSenderId: '2692045857',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
 };
--- src/app/services/libro.service.ts
+++ src/app/services/libro.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+
+
+interface Libro {
+  id?: string;
+  titulo: string;
+  autor: string;
+  genero: string;
+  editorial: string;
+  coleccion: string;
+  reimpresion: string;
+  estante: number;
+  portadaUrl?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class LibroService {
 
-  constructor() { }
+export class LibroService {
+  private librosCollection: any;
+  
+  constructor(private firestore: AngularFirestore, private storage: AngularFireStorage) {
+    this.librosCollection = this.firestore.collection<Libro>('libros');
+  }
+
+  getLibros(): Observable<Libro[]>{
+    return this.librosCollection.valueChages({idField: 'id'});
+  }
+
+  addLibro(libro: Libro, portada: File) {
+    const filePath = `portadas/${new Date().getTime()}_${portada.name}`;
+    const fileRef = this.storage.ref(filePath);
+    const uploadTask = this.storage.upload(filePath, portada);
+
+    return new Promise<void>((resolve, reject) => {
+      uploadTask.snapshotChanges().pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe(url => {
+            libro.portadaUrl = url;
+            this.librosCollection.add(libro).then(() => resolve()).catch(reject);
+          });
+        })
+      ).subscribe();
+    });
+  }
 }
