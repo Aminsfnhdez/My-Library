975fc8686d874502cffe9d96943018a484fde3d9
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-04-04 09:50:27 -0500

    corrección de uso variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCwZmUqxojDZG9bXML_bme3JXf-xvBI2sU",
-    authDomain: "my-library-caa60.firebaseapp.com",
-    projectId: "my-library-caa60",
-    storageBucket: "my-library-caa60.firebasestorage.app",
-    messagingSenderId: "2692045857",
-    appId: "1:2692045857:web:e6c2ce54a5ed4da1f99760"
+    apiKey: 'AIzaSyCwZmUqxojDZG9bXML_bme3JXf-xvBI2sU',
+    authDomain: 'my-library-caa60.firebaseapp.com',
+    projectId: 'my-library-caa60',
+    storageBucket: 'my-library-caa60.firebasestorage.app',
+    messagingSenderId: '2692045857',
+    appId: '1:2692045857:web:e6c2ce54a5ed4da1f99760'
     /*measurementId: "TU-MEASUREMENT-ID"*/
 
   }
